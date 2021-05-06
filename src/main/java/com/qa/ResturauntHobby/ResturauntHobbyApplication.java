cfba464ffa6c9a475aa929a953f88200177eba32
philipugona <PUgona@qa.com>
2021-05-06 16:08:50 +0100

    First commit. Created the README file and the Spring Project

---
+++ src/main/java/com/qa/ResturauntHobby/ResturauntHobbyApplication.java
@@ -0,0 +1,13 @@
+package com.qa.ResturauntHobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturauntHobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturauntHobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/ResturauntHobby/ResturauntHobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.ResturauntHobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturauntHobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
