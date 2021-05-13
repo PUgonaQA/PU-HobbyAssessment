4cfeecab255065e1d31f918eb0aaf7ee3c39ee44
philipugona <PUgona@qa.com>
2021-05-13 20:56:41 +0100

    New Restaurant Meals can be added and displayed in the table. Alerts created for invalid inputs

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -2,6 +2,13 @@
 
 const contextPath = "http://localhost:9092/";
 
+const RESTNAME = document.querySelector("#restName");
+const LOCATION = document.querySelector("#location");
+const DISH = document.querySelector("#dishName");
+const RATING = document.querySelector("#ratingNo");
+
+const CREATEALERT = document.querySelector("#createAlert")
+
 fetch("/getAll")
     .then(res => {
         res.json().then(
@@ -31,6 +38,53 @@ fetch("/getAll")
         )
     })
 
+// Create
+const createRest = () => {
 
+    const REST_NAME = RESTNAME.value;
+    const LOCATION_VALUE = LOCATION.value;
+    const DISH_VALUE = DISH.value;
+    const RATING_VALUE = RATING.value;
 
+    console.log(`${REST_NAME} ${LOCATION_VALUE} ${DISH_VALUE} ${RATING_VALUE}`);
 
+    let obj =
+    {
+        restaurantName: REST_NAME,
+        location: LOCATION_VALUE,
+        dish: DISH_VALUE,
+        rating: RATING_VALUE
+    };
+
+    //  Axios Post request
+    if ((REST_NAME == "") || (LOCATION_VALUE == "") || (DISH_VALUE == "") || (RATING_VALUE=="") || (RATING_VALUE>100) || (RATING_VALUE<0)) {
+        CREATEALERT.setAttribute("class", "alert alert-secondary");
+        CREATEALERT.innerHTML = "Restaurant entity not been created";
+        setTimeout(() => {
+            CREATEALERT.removeAttribute("class");
+            CREATEALERT.innerHTML = "";
+        }, 3000)
+    }
+    else {
+        axios
+            .post("/create", obj)
+            .then((resp) => {
+                console.log(resp);
+                CREATEALERT.setAttribute("class", "alert alert-secondary");
+                CREATEALERT.innerHTML = "Restaurant entity has been succesfully created";
+                setTimeout(() => {
+                    CREATEALERT.removeAttribute("class");
+                    CREATEALERT.innerHTML = "";
+                }, 3000)
+            })
+            .catch((err) => {
+                console.error(err);
+                CREATEALERT.setAttribute("class", "alert alert-secondary");
+                CREATEALERT.innerHTML = "Restaurant entity not been created";
+                setTimeout(() => {
+                    CREATEALERT.removeAttribute("class");
+                    CREATEALERT.innerHTML = "";
+                }, 3000)
+            })
+    }
+}
